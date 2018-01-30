b2628f6b3f7178a7376d605043f79a56fc3d52e6
Pavan Hegde <pavanrkadave@gmail.com>
2018-01-30 20:34:08 +0530

    => Added Firebase Authentication to the app => changed the app icon =>Bug Fixes & Performance Improvements.

--- app/src/main/java/io/github/pavanrkadave/rupeediary/ExpenseActivity.java
+++ app/src/main/java/io/github/pavanrkadave/rupeediary/ExpenseActivity.java
@@ -1,11 +1,21 @@
 package io.github.pavanrkadave.rupeediary;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,14 +35,24 @@ public class ExpenseActivity extends AppCompatActivity {
     private DatabaseReference expenseReference;
     private ProgressBar progressBar;
 
+    //Firebase User
+    String mUsername;
+
+    private FirebaseUser user;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense);
 
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        mUsername = user.getDisplayName();
+
         progressBar = findViewById(R.id.progress_bar);
 
-        expenseReference = FirebaseDatabase.getInstance().getReference("expenses");
+        expenseReference = FirebaseDatabase.getInstance().getReference("expenses").child(mUsername);
 
         expenseList = findViewById(R.id.expense_list);
 
@@ -66,6 +86,62 @@ public class ExpenseActivity extends AppCompatActivity {
         });
 
 
+        expenseList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                ExpenseObject obj = expensesList.get(i);
+                String desc = obj.getDescription();
+                showDeleteDialog(desc);
+
+                return false;
+            }
+        });
+
+
     }
+
+    private void showDeleteDialog(final String obj) {
+
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setMessage("Do you really want to delete the expense?")
+                .setTitle("Delete?");
+        alert.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                deleteData(obj);
+
+            }
+        });
+
+        alert.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        alert.create();
+        alert.show();
+        
+    }
+
+    private void deleteData(String obj) {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("expenses").child(mUsername).child(obj);
+        ref.removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Toast.makeText(ExpenseActivity.this, "Data deleted!", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(ExpenseActivity.this, "Error Deleting data!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
 }
 
--- app/src/main/java/io/github/pavanrkadave/rupeediary/MainActivity.java
+++ app/src/main/java/io/github/pavanrkadave/rupeediary/MainActivity.java
@@ -1,6 +1,7 @@
 package io.github.pavanrkadave.rupeediary;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,11 +13,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Locale;
 
@@ -28,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText descriptionInput;
     private Button saveData;
 
+
     //Intent to show the expenses
     Intent intent;
 
@@ -37,13 +44,21 @@ public class MainActivity extends AppCompatActivity {
 
     //Firebase Auth Variables
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthState;
 
+    //Current user info
+    public String mUsername;
+
+    //Static variables
+    private static final int RC_SIGN_IN = 1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
 
         //Expense Activity intent to display the expenses.
         intent = new Intent(MainActivity.this, ExpenseActivity.class);
@@ -66,7 +81,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mAuthState = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    //User is SignedIn.
+                    onSignedInInitialize(user.getDisplayName());
+                } else {
+                    //User is Not SignedIn.
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.EmailBuilder().build(),
+                                            new AuthUI.IdpConfig.GoogleBuilder().build()))
+                                    .setTheme(R.style.UITheme)
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+            };
+        }
+
+
+    private void onSignedInInitialize(String displayName) {
+        mUsername = displayName;
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -85,11 +127,20 @@ public class MainActivity extends AppCompatActivity {
 
             case R.id.show_expenses:
                 startActivity(intent);
+                break;
+            case R.id.sign_out: AuthUI.getInstance()
+                    .signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Toast.makeText(MainActivity.this, "Signed Out!", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+            break;
             default:
                 return super.onOptionsItemSelected(item);
-
         }
-
+        return true;
     }
 
     private void saveExpenses() {
@@ -99,13 +150,13 @@ public class MainActivity extends AppCompatActivity {
 
         //get the date and day as strings fro the Simpledate format class.
         String date = new SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(new Date());
-        String day = new SimpleDateFormat("EEEE",Locale.getDefault()).format(new Date());
+        String day = new SimpleDateFormat("EEEE", Locale.getDefault()).format(new Date());
 
         //If the text field is not empty save the data to firebase.
         if (!TextUtils.isEmpty(moneySpent) && !TextUtils.isEmpty(description)) {
             //Save the data
-            ExpenseObject newExpense = new ExpenseObject(moneySpent, description, date,day);
-            expenseReference.push().setValue(newExpense);
+            ExpenseObject newExpense = new ExpenseObject(moneySpent, description, date, day);
+            expenseReference.child(mUsername).push().setValue(newExpense);
             Toast.makeText(this, "Expense Added!", Toast.LENGTH_SHORT).show();
 
             //Clear the field once data is saved to cloud.
@@ -120,4 +171,36 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Both Fields Required!", Toast.LENGTH_SHORT).show();
         }
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+
+            if (resultCode == RESULT_OK) {
+
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                mUsername = user.getDisplayName();
+                Log.v("User",mUsername);
+
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mAuth.addAuthStateListener(mAuthState);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mAuthState != null) {
+            mAuth.removeAuthStateListener(mAuthState);
+        }
+    }
+
 }
